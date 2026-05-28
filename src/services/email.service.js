7b5b1a07cab545a27db6de86a91b9eb1c1783373
bingobknd2025 <200901390+bingobknd2025@users.noreply.github.com>
2026-05-28 12:19:42 +0530

    User create mail content update

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: true, 
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
 
 exports.sendEmail = async ({ to, subject, html, text }) => {
   return await transporter.sendMail({
-    from: `GOLD TO CASH <${process.env.SMTP_FROM}>`,
+    from: `BINGOLD <${process.env.SMTP_FROM}>`,
     to,
     subject,
     text,
